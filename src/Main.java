06c9a3828e95b52738b819da8bbbc991172ff540
dallascard <dcard@andrew.cmu.edu>
2016-09-22 17:02:26 -0700

    adjusting input params

--- src/Main.java
+++ src/Main.java
@@ -43,7 +43,7 @@ public class Main {
 
         CombinedModel sampler = new CombinedModel(documents, metadata, predictions, mood, normalizeStoriesAtTime, normalizeMood);
 
-        sampler.run(500, 400, 10, 100);
+        sampler.run(6000, 4000, 10, 100);
 
     }
 }
